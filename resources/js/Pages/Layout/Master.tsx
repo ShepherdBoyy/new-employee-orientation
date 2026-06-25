114ef99232f97a45408ac3b63cc7da1d0b04328c
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-06-25 14:28:03 +0800

    Update Master.tsx
    Created Master component for creating a generic layout through all pages

--- resources/js/Pages/Layout/Master.tsx
+++ resources/js/Pages/Layout/Master.tsx
@@ -1,9 +1,29 @@
-export default function Master({}) {
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { AppSidebar } from "./AppSidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+type MasterProps = {
+    children: React.ReactNode;
+};
+
+export default function Master({ children }: MasterProps) {
     return (
         <>
-            <div className="h-18 bg-red-50 flex items-center justify-end px-4">
-                <span>Testing</span>
-            </div>
+            <TooltipProvider>
+                <div>
+                    <SidebarProvider>
+                        <AppSidebar />
+                        <div className="w-full">
+                            <header>
+                                <div className="h-14 px-4 py-6 flex items-center border-b w-full">
+                                    <SidebarTrigger />
+                                </div>
+                            </header>
+
+                            <main className="flex-1 p-6">{children}</main>
+                        </div>
+                    </SidebarProvider>
+                </div>
+            </TooltipProvider>
         </>
     );
 }
