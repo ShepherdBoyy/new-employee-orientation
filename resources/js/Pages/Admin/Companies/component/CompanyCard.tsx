209cb5ae707e62e6c2cfe294194c2e3bfb6a0760
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-07-07 10:25:20 +0800

    Update CompanyCard.tsx
    Added a fallback image in the logo of the company incase there is no picture uploaded

--- resources/js/Pages/Admin/Companies/component/CompanyCard.tsx
+++ resources/js/Pages/Admin/Companies/component/CompanyCard.tsx
@@ -1,6 +1,7 @@
 import { Company, COMPANY_THEMES } from "../Index";
 import CompanyCardActions from "./CompanyCardActions";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 type CompanyCardProps = {
     company: Company;
@@ -33,11 +34,10 @@ export default function CompanyCard({
                     </h2>
                 </div>
 
-                <img
-                    src={`/storage/${company.logo_path}`}
-                    alt={company.name}
-                    className="absolute -bottom-8 right-6 h-16 w-16 rounded-full border-4 border-background object-cover bg-background z-10"
-                />
+                <Avatar className="absolute -bottom-8 right-6 h-16 w-16 rounded-full border-4 border-background object-cover bg-background z-10">
+                    <AvatarImage src={`/storage/${company.logo_path}`} />
+                    <AvatarFallback> {company.name[0]}</AvatarFallback>
+                </Avatar>
             </div>
             <CardHeader>
                 <CardContent className="pt-12">
