cd7cce889455758bf5531d94b48a0f53cec997b3
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-07-29 15:03:25 +0800

    Update Master.tsx

--- resources/js/Layout/Master.tsx
+++ resources/js/Layout/Master.tsx
@@ -4,18 +4,33 @@ import {
     SidebarProvider,
     SidebarTrigger,
     SidebarInset,
+    SidebarMenuButton,
 } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
-import { usePage } from "@inertiajs/react";
-import { Toaster } from "@/components/ui/sonner"
+import { Link, router, usePage } from "@inertiajs/react";
+import { Toaster } from "@/components/ui/sonner";
 import NotificationBell from "./NotificationBell";
+import {
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuItem,
+    DropdownMenu,
+} from "@/components/ui/dropdown-menu";
+
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { ChevronsUpDown, LogOut } from "lucide-react";
 
 type MasterProps = {
     children: React.ReactNode;
+    id: number;
+    name: string;
+    email: string;
 };
-
-export default function Master({ children }: MasterProps) {
+export default function Master({ children, id, name, email }: MasterProps) {
     const url = usePage();
+    const user = usePage().props.auth.user as MasterProps;
     return (
         <>
             <TooltipProvider>
@@ -23,12 +38,94 @@ export default function Master({ children }: MasterProps) {
                     <SidebarProvider>
                         <AppSidebar />
                         <SidebarInset>
-                            <header className="flex h-14 px-4 border-b shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 ">
-                                <div className="flex items-center gap-2 mt-2">
+                            <header className="sticky top-0 z-40 flex h-16 items-center justify-between border-b bg-background px-6">
+                                {/* Left */}
+                                <div className="flex items-center gap-3">
                                     <SidebarTrigger />
-                                    <Separator orientation="vertical" />
+
+                                    <Separator
+                                        orientation="vertical"
+                                        className=""
+                                    />
+
+                                    {/* Optional breadcrumb/page title */}
+                                    <h1 className="text-sm font-medium text-muted-foreground">
+                                        Administration
+                                    </h1>
                                 </div>
-                                <NotificationBell />
+
+                                {/* Right */}
+                                <div className="flex items-center gap-4">
+                                    <NotificationBell />
+
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger
+                                            className="w-60"
+                                            asChild
+                                        >
+                                            <SidebarMenuButton className="">
+                                                <Avatar>
+                                                    <AvatarImage />
+                                                    <AvatarFallback className="rounded-lg">
+                                                        {user.name
+                                                            .slice(0, 2)
+                                                            .toUpperCase()}
+                                                    </AvatarFallback>
+                                                </Avatar>
+                                                <div className="grid flex-1 text-left text-sm leading-tight">
+                                                    <span className="truncate font-medium">
+                                                        {user.name}
+                                                    </span>
+                                                    <span className="truncate text-xs">
+                                                        {user.email}
+                                                    </span>
+                                                </div>
+                                                <ChevronsUpDown className="ml-auto size-4" />
+                                            </SidebarMenuButton>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent
+                                            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                                            align="end"
+                                            side="bottom"
+                                            sideOffset={6}
+                                        >
+                                            <DropdownMenuLabel className="p-0 font-normal">
+                                                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                                    <Avatar className="h-8 w-8 rounded-lg">
+                                                        <AvatarImage />
+                                                        <AvatarFallback className="rounded-lg">
+                                                            {user.name
+                                                                .slice(0, 2)
+                                                                .toUpperCase()}
+                                                        </AvatarFallback>
+                                                    </Avatar>
+                                                    <div className="grid flex-1 text-left text-sm leading-tight">
+                                                        <span className="truncate font-medium">
+                                                            {user.name}
+                                                        </span>
+                                                        <span className="truncate text-xs">
+                                                            {user.email}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem
+                                                onClick={() =>
+                                                    router.get("/logout")
+                                                }
+                                            >
+                                                <Link
+                                                    href="/logout"
+                                                    className="flex gap-2 items-center justify-between"
+                                                >
+                                                    <LogOut />
+                                                    <span>Log out</span>
+                                                </Link>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </div>
                             </header>
 
                             <main className="flex-1 p-12">{children}</main>
