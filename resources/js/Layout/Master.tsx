849507ed282c8fc95248437a7cf469a4ccbf9705
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-08-04 08:39:51 +0800

    Update Master.tsx

--- resources/js/Layout/Master.tsx
+++ resources/js/Layout/Master.tsx
@@ -36,88 +36,59 @@ export default function Master({ children }: MasterProps) {
     return (
         <>
             <TooltipProvider>
-                <div className="flex flex-1 overflow-hidden">
-                    <SidebarProvider className="">
+                <div className="">
+                    <SidebarProvider className="h-svh overflow-hidden bg-[#FEFEFA]">
                         <AppSidebar />
-                        <AnimatePresence mode="wait">
-                            {showPresentationSidebar && (
-                                <motion.div
-                                    key="presentation-panel"
-                                    initial={{
-                                        x: -24,
-                                        opacity: 0,
-                                    }}
-                                    animate={{
-                                        x: 0,
-                                        opacity: 1,
-                                    }}
-                                    exit={{
-                                        x: -24,
-                                        opacity: 0,
-                                    }}
-                                    transition={{
-                                        duration: 0.22,
-                                        ease: "easeOut",
-                                    }}
-                                    className=""
-                                >
-                                    <PresentationPanel />
-                                </motion.div>
-                            )}
-                        </AnimatePresence>
+                        <div className="flex flex-1 gap-2  p-2">
+                            <AnimatePresence mode="wait">
+                                {showPresentationSidebar && (
+                                    <motion.div
+                                        className="px-1"
+                                        key="presentation-panel"
+                                        initial={{
+                                            x: -24,
+                                            opacity: 0,
+                                        }}
+                                        animate={{
+                                            x: 0,
+                                            opacity: 1,
+                                        }}
+                                        exit={{
+                                            x: -24,
+                                            opacity: 0,
+                                        }}
+                                        transition={{
+                                            duration: 0.22,
+                                            ease: "easeOut",
+                                        }}
+                                    >
+                                        <PresentationPanel />
+                                    </motion.div>
+                                )}
+                            </AnimatePresence>
 
-                        <div className="flex-1 flex flex-col overflow-hidden  bg-background rounded-xl shadow-lg">
-                            <header className="sticky top-0 z-40 flex h-16 items-center justify-between border-b bg-background px-6">
-                                <div className="flex items-center gap-3">
-                                    <SidebarTrigger />
+                            <div className="flex h-full flex-1 flex-col overflow-hidden rounded-xl  bg-background border">
+                                <header className="sticky top-0 z-40 flex h-16 items-center justify-between border-b bg-background px-6">
+                                    <div className="flex items-center gap-3">
+                                        <SidebarTrigger />
 
-                                    <Separator
-                                        orientation="vertical"
-                                        className=""
-                                    />
+                                        <Separator orientation="vertical" />
 
-                                    <h1 className="text-sm font-medium text-muted-foreground">
-                                        Administration
-                                    </h1>
-                                </div>
+                                        <h1 className="text-sm font-medium text-muted-foreground">
+                                            Administration
+                                        </h1>
+                                    </div>
 
-                                <div className="flex items-center gap-4">
-                                    <NotificationBell />
+                                    <div className="flex items-center gap-4">
+                                        <NotificationBell />
 
-                                    <DropdownMenu>
-                                        <DropdownMenuTrigger
-                                            className="w-60"
-                                            asChild
-                                        >
-                                            <SidebarMenuButton className="">
-                                                <Avatar>
-                                                    <AvatarImage />
-                                                    <AvatarFallback className="rounded-lg">
-                                                        {user.name
-                                                            .slice(0, 2)
-                                                            .toUpperCase()}
-                                                    </AvatarFallback>
-                                                </Avatar>
-                                                <div className="grid flex-1 text-left text-sm leading-tight">
-                                                    <span className="truncate font-medium">
-                                                        {user.name}
-                                                    </span>
-                                                    <span className="truncate text-xs">
-                                                        {user.email}
-                                                    </span>
-                                                </div>
-                                                <ChevronsUpDown className="ml-auto size-4" />
-                                            </SidebarMenuButton>
-                                        </DropdownMenuTrigger>
-                                        <DropdownMenuContent
-                                            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-                                            align="end"
-                                            side="bottom"
-                                            sideOffset={6}
-                                        >
-                                            <DropdownMenuLabel className="p-0 font-normal">
-                                                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                                                    <Avatar className="h-8 w-8 rounded-lg">
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger
+                                                className="w-60"
+                                                asChild
+                                            >
+                                                <SidebarMenuButton className="">
+                                                    <Avatar>
                                                         <AvatarImage />
                                                         <AvatarFallback className="rounded-lg">
                                                             {user.name
@@ -133,32 +104,60 @@ export default function Master({ children }: MasterProps) {
                                                             {user.email}
                                                         </span>
                                                     </div>
-                                                </div>
-                                            </DropdownMenuLabel>
-                                            <DropdownMenuSeparator />
-                                            <DropdownMenuItem
-                                                onClick={() =>
-                                                    router.get("/logout")
-                                                }
+                                                    <ChevronsUpDown className="ml-auto size-4" />
+                                                </SidebarMenuButton>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent
+                                                className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                                                align="end"
+                                                side="bottom"
+                                                sideOffset={6}
                                             >
-                                                <Link
-                                                    href="/logout"
-                                                    className="flex gap-2 items-center justify-between"
+                                                <DropdownMenuLabel className="p-0 font-normal">
+                                                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                                        <Avatar className="h-8 w-8 rounded-lg">
+                                                            <AvatarImage />
+                                                            <AvatarFallback className="rounded-lg">
+                                                                {user.name
+                                                                    .slice(0, 2)
+                                                                    .toUpperCase()}
+                                                            </AvatarFallback>
+                                                        </Avatar>
+                                                        <div className="grid flex-1 text-left text-sm leading-tight">
+                                                            <span className="truncate font-medium">
+                                                                {user.name}
+                                                            </span>
+                                                            <span className="truncate text-xs">
+                                                                {user.email}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </DropdownMenuLabel>
+                                                <DropdownMenuSeparator />
+                                                <DropdownMenuItem
+                                                    onClick={() =>
+                                                        router.get("/logout")
+                                                    }
                                                 >
-                                                    <LogOut />
-                                                    <span>Log out</span>
-                                                </Link>
-                                            </DropdownMenuItem>
-                                        </DropdownMenuContent>
-                                    </DropdownMenu>
-                                </div>
-                            </header>
+                                                    <Link
+                                                        href="/logout"
+                                                        className="flex gap-2 items-center justify-between"
+                                                    >
+                                                        <LogOut />
+                                                        <span>Log out</span>
+                                                    </Link>
+                                                </DropdownMenuItem>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                    </div>
+                                </header>
 
-                            <main className="flex-1 overflow-y-auto p-8">
-                                {children}
-                            </main>
+                                <main className="flex-1 overflow-y-auto p-8">
+                                    {children}
+                                </main>
 
-                            <Toaster />
+                                <Toaster />
+                            </div>
                         </div>
                     </SidebarProvider>
                 </div>
