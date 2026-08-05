09dbfa84715bd2a35f30772bd855e709f88377ea
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-08-05 13:58:40 +0800

    Create TopicCard.tsx

---
+++ resources/js/Pages/Admin/Topics/component/TopicCard.tsx
@@ -0,0 +1,34 @@
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+interface Topics {
+    id: number;
+    label: string;
+    slug: string;
+    slides_count: number;
+}
+
+interface Props {
+    topics: Topics[];
+}
+
+export default function TopicCard({ topics }: Props) {
+    return (
+        <div>
+            {topics.map((topic) => (
+                <Card key={topic.id}>
+                    <CardHeader>
+                        <CardTitle>{topic.label}</CardTitle>
+                        <CardDescription>
+                            {topic.slides_count} slides
+                        </CardDescription>
+                    </CardHeader>
+                </Card>
+            ))}
+        </div>
+    );
+}
